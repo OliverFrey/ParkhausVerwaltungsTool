8ff730de1ee003ecc4d9d703829eef0cd408d8ef
Oliver Frey <oliver.frey@geotest.ch>
2023-01-16 08:15:19 +0100

    update-database

--- ParkplatzVerwaltungsTool/Models/ParkingHouseContext.cs
+++ ParkplatzVerwaltungsTool/Models/ParkingHouseContext.cs
@@ -17,39 +17,6 @@ namespace ParkplatzVerwaltungsTool.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite($"Data Source={DbPath}");
+
     }
-
-    //public class ParkingHouseLevelContext : DbContext
-    //{
-    //    public DbSet<ParkingHouse> ParkingHouses { get; set; }
-
-    //    public string DbPath { get; }
-
-    //    public ParkingHouseLevelContext()
-    //    {
-    //        var folder = Environment.SpecialFolder.LocalApplicationData;
-    //        var path = Environment.GetFolderPath(folder);
-    //        DbPath = "parkingHouseDb.db";
-    //    }
-
-    //    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //        => options.UseSqlite($"Data Source={DbPath}");
-    //}
-
-    //public class ParkingPlaceContext : DbContext
-    //{
-    //    public DbSet<ParkingHouse> ParkingHouses { get; set; }
-
-    //    public string DbPath { get; }
-
-    //    public ParkingPlaceContext()
-    //    {
-    //        var folder = Environment.SpecialFolder.LocalApplicationData;
-    //        var path = Environment.GetFolderPath(folder);
-    //        DbPath = "parkingHouseDb.db";
-    //    }
-
-    //    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    //        => options.UseSqlite($"Data Source={DbPath}");
-    //}
-}
+    }
